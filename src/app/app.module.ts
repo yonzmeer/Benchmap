78aa85750f44c8971083a7cfd7146ad56276c186
yonzmeer <meersonj@gmail.com>
2020-07-19 21:47:07 +0300

    made boo boo with pipe import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { TargetsModule } from 'projects/targets/src/public-api';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    SharedModule,
     TargetsModule.forRoot(),
   ],
   providers: [],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Enum2StringPipe } from './enum-2-string.pipe';
 
+
+
 @NgModule({
   declarations: [
     Enum2StringPipe
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    Enum2StringPipe
   ]
 })
 export class SharedModule { }
