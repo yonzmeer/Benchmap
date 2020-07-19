f431ca1cbaefcd94a992e3e7d7020c96d7382c33
yonzmeer <meersonj@gmail.com>
2020-07-19 21:43:01 +0300

    move enum2string pipe to shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { TargetsModule } from 'projects/targets/src/public-api';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Enum2StringPipe } from './enum-2-string.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Enum2StringPipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Enum2StringPipe } from './enum-2-string.pipe';
+
+@NgModule({
+  declarations: [
+    Enum2StringPipe
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
